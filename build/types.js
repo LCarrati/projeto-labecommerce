9f7133031e15388da55349c7bed119d49ace4e27
Leo Carrati <carrati3@gmail.com>
2023-04-21 21:33:07 -0300

    Atualizar pasta BUILD

--- build/types.js
+++ build/types.js
@@ -1,10 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.CATEGORIES = void 0;
-var CATEGORIES;
-(function (CATEGORIES) {
-    CATEGORIES["ACCESSORIES"] = "Acess\u00F3rios";
-    CATEGORIES["CLOTHES_AND_SHOES"] = "Roupas e cal\u00E7ados";
-    CATEGORIES["ELETROS"] = "Eletr\u00F4nicos";
-})(CATEGORIES = exports.CATEGORIES || (exports.CATEGORIES = {}));
 //# sourceMappingURL=types.js.map
